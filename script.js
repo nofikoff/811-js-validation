dfd2f9ef2097d76a730207bc82034bfa55d4d443
nofikoffgit <ruslan.novikov@gmail.com>
2020-05-19 20:56:19 +0300

    Валидация

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
-
-
 if (typeof regForm !== 'undefined') {
     console.log("Форма регистрации")
 
     // если вход
     myEnter.addEventListener("click", (e) => {
-        console.log("Выход");
+        console.log("Вход");
+        /*
         //e.preventDefault();
         setCookie("email", "ruslan.novikov@gmail.com", 1)
         setCookie("password", md5("1234"), 1)
         // на личный кабинет
         window.location.href = "./bio.html";
-    })
+        */
+
+        try {
+
+            if (!validateEmail(email.value))
+                throw ("Email " + email.value + " is not valid");
+
+            if (!validatePassword(password.value, password2.value,))
+                throw ("Password " + password.value + " is not valid");
+
+            result.innerHTML = " ОЛ РАЙТ";
+
+
+        } catch (e) {
+            // statements to handle any exceptions
+            console.log(e); // pass exception object to error handler
+            result.innerHTML = e;
+        }
+
+
+    });
 
 
     // если авторизован - bio.html
@@ -19,6 +38,17 @@ if (typeof regForm !== 'undefined') {
         window.location.href = "./bio.html";
     }
 
+
+    function validatePassword(p1, p2) {
+        if (p1 === '') return false;
+        //let re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return p1 === p2;
+    }
+
+    function validateEmail(email) {
+        let re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    }
 
 
 }
