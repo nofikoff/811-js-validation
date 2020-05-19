0240ddc116979ea7540b522807b77de90c33576a
nofikoffgit <ruslan.novikov@gmail.com>
2020-05-19 18:42:23 +0300

    опкбликовал на https://nofikoff.github.io/811-js-validation/

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+if (typeof regForm !== 'undefined') {
+    console.log("Форма регистрации")
+
+
+
+}
+
+if (typeof bioForm !== 'undefined') {
+    console.log("Форма личный кабинет")
+
+
+
+
+}
